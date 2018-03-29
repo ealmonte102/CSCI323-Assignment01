dd0addb4df0a5f6d825130e2149b9b54373959e1
Evan Almonte <ealmonte102@qc.cuny.edu>
2018-03-29 08:14:24 -0400

    Create the RadixSorter class which sorts using the LSD version of Radix Sort
    - Add unit test for the sort method using randomly chosen integers

---
+++ test/com/alev/sorter/RadixSorterTest.java
@@ -0,0 +1,23 @@
+package com.alev.sorter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class RadixSorterTest {
+    private RadixSorter radixSorter;
+
+    @Before
+    public void setUp() {
+        radixSorter = new RadixSorter();
+    }
+
+    @Test
+    public void sort() {
+        int[] a = {35, 24, 53, 42, 11, 50, 2, 1, 9, 8};
+        radixSorter.setArray(a);
+        radixSorter.sort();
+        assertArrayEquals(new int[]{1, 2, 8, 9, 11, 24, 35, 42, 50, 53}, radixSorter.getArray());
+    }
+}
