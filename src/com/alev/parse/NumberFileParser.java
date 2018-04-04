4727aca0f9721334e32671acc810b5dc3fe267d0
Evan Almonte <ealmonte102@qc.cuny.edu>
2018-04-04 16:44:52 -0400

    Refactor NumberFileParser to use File class vs String
    The NumberFileParser class now represents the current file using the
    Java.io.File class instead of the String class. Change made to reverse
    the dependency on creating the file within the getParsedArray function.

--- src/com/alev/parse/NumberFileParser.java
+++ src/com/alev/parse/NumberFileParser.java
@@ -1,34 +1,21 @@
 package com.alev.parse;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class NumberFileParser {
-    private String filename;
-    private BufferedReader reader;
-
+    public static final String DATA_FILE_PREFIX = "Num";
+    public static final String DATA_FILE_SUFFIX = ".txt";
+    private static final String DATA_FILE_REGEX = DATA_FILE_PREFIX + "\\d{1,5}" + DATA_FILE_SUFFIX;
+    private File dataFile;
     private int[] parsedArray;
 
-    public int[] readFile() throws FileNotFoundException {
+    public int[] getParsedArray() {
         if(parsedArray == null) {
-            ArrayList<Integer> tempArrayList = new ArrayList<>();
-            reader = new BufferedReader(new FileReader(new File(filename)));
-            String currentLine;
-            try {
-                while ((currentLine = reader.readLine()) != null) {
-                    tempArrayList.add(Integer.parseInt(currentLine));
-                }
-            } catch (NumberFormatException ignored) {
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            ArrayList<Integer> tempArrayList = parseFile();
             parsedArray = new int[tempArrayList.size()];
             for (int i = 0; i < parsedArray.length; ++i) {
                 parsedArray[i] = tempArrayList.get(i);
@@ -37,8 +24,32 @@ public class NumberFileParser {
         return parsedArray;
     }
 
-    public void setFilename(String filename) {
-        this.filename = filename;
-        this.parsedArray = null;
+    public void setFile(File file) throws IllegalArgumentException {
+        if (!file.isFile()) {
+            throw new IllegalArgumentException("File provided is not a file");
+        }
+        if (!file.getName().matches(DATA_FILE_REGEX)) {
+            throw new IllegalArgumentException(
+                    String.format("File name: %s is improperly named", file.getName()));
+        }
+        dataFile = file;
+        parsedArray = null;
     }
+
+    private ArrayList<Integer> parseFile() {
+        ArrayList<Integer> tempArrayList = new ArrayList<>();
+        try {
+            try (BufferedReader reader = new BufferedReader(new FileReader(dataFile))) {
+                String currentLine;
+                while ((currentLine = reader.readLine()) != null) {
+                    tempArrayList.add(Integer.parseInt(currentLine));
+                }
+            } catch (NumberFormatException ignored) {
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tempArrayList;
+    }
+
 }
--- test/com/alev/sorter/NumberFileParserTests.java
+++ test/com/alev/sorter/NumberFileParserTests.java
@@ -4,11 +4,9 @@ import com.alev.parse.NumberFileParser;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.File;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
 public class NumberFileParserTests {
     private NumberFileParser parser;
@@ -19,10 +17,10 @@ public class NumberFileParserTests {
     }
 
     @Test
-    public void readFile_fileEndsWithNewLines_ArrayWithIntegersReturned() throws FileNotFoundException {
-        parser.setFilename("data/Num8.txt");
-        int[] integerArray = parser.readFile();
-        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(2, 8, 3, 1, 7, 6, 5, 4));
-        assertTrue(integerArray.equals(expected));
+    public void readFile_fileEndsWithNewLines_ArrayWithIntegersReturned() {
+        parser.setFile(new File("numfiles/data1/Num8.txt"));
+        int[] integerArray = parser.getParsedArray();
+        int[] expected = {2, 8, 3, 1, 7, 6, 5, 4};
+        assertArrayEquals(expected, integerArray);
     }
 }
