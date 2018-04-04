5efd9765ad9ebcb2aba1b27cd6ae59c8d1bf7584
Evan Almonte <ealmonte102@qc.cuny.edu>
2018-04-03 23:51:08 -0400

    Add class to complete part one of assignment #1

---
+++ src/com/alev/parse/PartOneAnalyzer.java
@@ -0,0 +1,11 @@
+package com.alev.parse;
+
+import java.io.File;
+
+public class PartOneAnalyzer {
+    File directoryLocation;
+
+    public PartOneAnalyzer(String directoryLocation) {
+        this.directoryLocation = new File(directoryLocation);
+    }
+}
