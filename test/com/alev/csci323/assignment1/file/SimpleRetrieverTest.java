48bc41a07f451fe26a9fa8d054f910f73ece40f5
Evan Almonte <ealmonte102@qc.cuny.edu>
2018-04-06 11:46:53 -0400

    Remove excepition handling from test method which was unneeded.

--- test/com/alev/csci323/assignment1/file/SimpleRetrieverTest.java
+++ test/com/alev/csci323/assignment1/file/SimpleRetrieverTest.java
@@ -3,22 +3,16 @@ package com.alev.csci323.assignment1.file;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 
 import static org.junit.Assert.assertArrayEquals;
 
 public class SimpleRetrieverTest {
-    private SimpleRetriever retriever;
 
     @Test
-    public void retrieveDataFiles() {
-        retriever = new SimpleRetriever("./numfiles/data1/");
+    public void retrieveDataFiles_filesExist_allFilesRetrieved() {
+        SimpleRetriever retriever = new SimpleRetriever("./numfiles/data1/");
         File[] retrievedFiles = new File[0];
-        try {
-            retrievedFiles = retriever.retrieveDataFiles();
-        } catch (FileNotFoundException fne) {
-            fne.printStackTrace();
-        }
+        retrievedFiles = retriever.retrieveDataFiles();
         String[] expectedFilePaths = new String[]{
                 "./numfiles/data1/Num8.txt", "./numfiles/data1/Num16.txt", "./numfiles/data1/Num32.txt",
                 "./numfiles/data1/Num64.txt", "./numfiles/data1/Num128.txt", "./numfiles/data1/Num256.txt",
