1739f938329f2bb9696821d44db3f180cb347bbe
Evan Almonte <ealmonte102@qc.cuny.edu>
2018-04-05 20:04:37 -0400

    Refactor Part1Analyzer to take in an array of sorters as a parameter
    Support reuse for part 2 of the assignment

--- src/com/alev/analysis/Assignment1Analyzer.java
+++ src/com/alev/analysis/Assignment1Analyzer.java
@@ -3,42 +3,31 @@ package com.alev.analysis;
 import com.alev.file.Assignment1CSVWriter;
 import com.alev.file.Assignment1FileRetriever;
 import com.alev.parse.NumberFileParser;
-import com.alev.sorter.*;
+import com.alev.sorter.Sorter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
-public class PartOneAnalyzer {
-    private static final String ANALYZER_NAME = "part1analysis.txt";
-    private Assignment1FileRetriever fileRetriever;
-    private Assignment1CSVWriter csvWriter;
+public class Assignment1Analyzer {
+    private final Assignment1FileRetriever fileRetriever;
+    private final Assignment1CSVWriter csvWriter;
+    private final Sorter[] sorters;
 
-    private Sorter[] sorters = {
-            new InsertionSorter(),
-            new MergeSorter(),
-            new HeapSorter(),
-            new QuickSorter()};
-
-    public PartOneAnalyzer(Assignment1FileRetriever fileRetriever, Assignment1CSVWriter csvWriter) {
+    public Assignment1Analyzer(Sorter[] sorters, Assignment1FileRetriever fileRetriever, Assignment1CSVWriter csvWriter) {
         this.fileRetriever = Objects.requireNonNull(fileRetriever);
         this.csvWriter = Objects.requireNonNull(csvWriter);
+        this.sorters = Objects.requireNonNull(sorters);
     }
 
     public void executeAnalysis() {
         NumberFileParser parser = new NumberFileParser();
         try {
-            ArrayList<String> sorterNames = new ArrayList<>();
-            ArrayList<String> fileNames = new ArrayList<>();
-            ArrayList<String> count = new ArrayList<>();
-            ArrayList<String> sortedResult = new ArrayList<>();
             for (File file : fileRetriever.retrieveDataFiles()) {
                 parser.setFile(file);
                 for (Sorter sorter : sorters) {
                     sorter.setArray(parser.getParsedArray());
-
                     System.out.printf("Sorter Type: %s%n", sorter.getSorterType());
                     System.out.printf("Count: %d%n", sorter.sort());
                     System.out.printf("Array: %s%n", Arrays.toString(sorter.getArray()));
