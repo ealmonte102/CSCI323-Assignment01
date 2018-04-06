7e8aaab866365a68ffad48e6b3abc2350701f616
Evan Almonte <ealmonte102@qc.cuny.edu>
2018-04-06 10:52:47 -0400

    Fix executeAnalysis to save results of sorts to a file

--- src/com/alev/csci323/assignment1/analysis/Analyzer.java
+++ src/com/alev/csci323/assignment1/analysis/Analyzer.java
@@ -6,7 +6,9 @@ import com.alev.csci323.assignment1.parse.NumberFileParser;
 import com.alev.csci323.assignment1.sorter.Sorter;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -15,26 +17,33 @@ public class Analyzer {
     private final CSVWriter csvWriter;
     private final Sorter[] sorters;
 
-    public Analyzer(Sorter[] sorters, FileRetriever fileRetriever, CSVWriter csvWriter) {
+    public Analyzer(Sorter[] sorters,
+                    FileRetriever fileRetriever,
+                    CSVWriter csvWriter) {
         this.fileRetriever = Objects.requireNonNull(fileRetriever);
         this.csvWriter = Objects.requireNonNull(csvWriter);
         this.sorters = Objects.requireNonNull(sorters);
     }
 
-    public void executeAnalysis() {
+    public void executeAnalysis(String name) {
         NumberFileParser parser = new NumberFileParser();
-        try {
+        try (PrintWriter pw = new PrintWriter(new FileOutputStream(new File(name)))) {
             for (File file : fileRetriever.retrieveDataFiles()) {
                 parser.setFile(file);
                 for (Sorter sorter : sorters) {
                     sorter.setArray(parser.getParsedArray());
-                    System.out.printf("Sorter Type: %s%n", sorter.getSorterType());
-                    System.out.printf("Count: %d%n", sorter.sort());
-                    System.out.printf("Array: %s%n", Arrays.toString(sorter.getArray()));
-
+                    csvWriter.addResult(
+                            file.getName(),
+                            new CSVWriter.Assignment1Result(
+                                    sorter.getSorterType(),
+                                    sorter.sort(),
+                                    Arrays.toString(sorter.getArray())
+                            )
+                    );
                 }
             }
-        } catch (FileNotFoundException e) {
+            csvWriter.writeCSVFile(pw);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
