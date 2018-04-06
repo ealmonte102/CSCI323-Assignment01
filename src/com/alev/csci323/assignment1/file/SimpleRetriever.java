f6f87ffd64a54843f67d3bfabd2e8d2bbb195192
Evan Almonte <ealmonte102@qc.cuny.edu>
2018-04-06 11:44:40 -0400

    Remove code to create file name dynamically using bases and exponents

--- src/com/alev/csci323/assignment1/file/SimpleRetriever.java
+++ src/com/alev/csci323/assignment1/file/SimpleRetriever.java
@@ -1,45 +1,38 @@
 package com.alev.csci323.assignment1.file;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class SimpleRetriever implements FileRetriever {
     public static final String FILE_PREFIX = "Num";
     public static final String FILE_SUFFIX = ".txt";
-    public static final int BASE_TWO = 2;
 
     private File directoryLocation;
     private File files[];
-    private int startExponent;
-    private int endExponent;
 
-    public SimpleRetriever(String directoryLocation, int
-            startExponent, int endExponent)
+    public SimpleRetriever(String directoryLocation)
             throws IllegalArgumentException {
         this.directoryLocation = new File(directoryLocation);
         if (!this.directoryLocation.isDirectory()) {
             throw new IllegalArgumentException("File provided is not a directory");
         }
-        if (startExponent < endExponent) {
-            this.startExponent = endExponent;
-            this.endExponent = startExponent;
-        }
     }
-
     @Override
-    public File[] retrieveDataFiles() throws FileNotFoundException {
-        if (files == null) {
-            files = new File[endExponent - startExponent];
-            for (int i = startExponent; i < endExponent; ++i) {
-                String fileName = String.format("%s%d%s", FILE_PREFIX, (int)
-                        Math.pow(BASE_TWO, i), FILE_SUFFIX);
-                File createdFile = new File(directoryLocation, fileName);
-                if (!createdFile.exists()) {
-                    throw new FileNotFoundException(fileName + " not found in directory " + directoryLocation.getName());
-                }
-                files[i - startExponent] = new File(directoryLocation, fileName);
+    public File[] retrieveDataFiles() {
+        files = directoryLocation.listFiles();
+        Arrays.sort(files, new Comparator<File>() {
+            @Override
+            public int compare(File o1, File o2) {
+                String fileNum1 = o1.getName();
+                fileNum1 = fileNum1.substring(FILE_PREFIX.length(),
+                        fileNum1.length() - FILE_SUFFIX.length());
+                String fileNum2 = o2.getName();
+                fileNum2 = fileNum2.substring(FILE_PREFIX.length(),
+                        fileNum2.length() - FILE_SUFFIX.length());
+                return Integer.parseInt(fileNum1) - Integer.parseInt(fileNum2);
             }
-        }
+        });
         return files;
     }
 
