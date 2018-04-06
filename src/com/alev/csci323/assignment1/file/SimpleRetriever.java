1dc9967fff4cb9afdc5bd5b8a0680113861a3902
Evan Almonte <ealmonte102@qc.cuny.edu>
2018-04-06 11:07:47 -0400

    Change SimpleRetriever to accept a supplied integer range

--- src/com/alev/csci323/assignment1/file/SimpleRetriever.java
+++ src/com/alev/csci323/assignment1/file/SimpleRetriever.java
@@ -6,32 +6,42 @@ import java.io.FileNotFoundException;
 public class SimpleRetriever implements FileRetriever {
     public static final String FILE_PREFIX = "Num";
     public static final String FILE_SUFFIX = ".txt";
-    private static final int STARTING_EXPONENT = 3;
-    private static final int ENDING_EXPONENT = 15;
-    private static final int BASE = 2;
+    public static final int BASE_TWO = 2;
+
     private File directoryLocation;
     private File files[];
+    private int startExponent;
+    private int endExponent;
 
-    public SimpleRetriever(String directoryLocation) throws IllegalArgumentException {
+    public SimpleRetriever(String directoryLocation, int
+            startExponent, int endExponent)
+            throws IllegalArgumentException {
         this.directoryLocation = new File(directoryLocation);
         if (!this.directoryLocation.isDirectory()) {
             throw new IllegalArgumentException("File provided is not a directory");
+        }
+        if (startExponent < endExponent) {
+            this.startExponent = endExponent;
+            this.endExponent = startExponent;
         }
     }
 
     @Override
     public File[] retrieveDataFiles() throws FileNotFoundException {
         if (files == null) {
-            files = new File[ENDING_EXPONENT - STARTING_EXPONENT];
-            for (int i = STARTING_EXPONENT; i < ENDING_EXPONENT; ++i) {
-                String fileName = String.format("%s%d%s", FILE_PREFIX, (int) Math.pow(BASE, i), FILE_SUFFIX);
+            files = new File[endExponent - startExponent];
+            for (int i = startExponent; i < endExponent; ++i) {
+                String fileName = String.format("%s%d%s", FILE_PREFIX, (int)
+                        Math.pow(BASE_TWO, i), FILE_SUFFIX);
                 File createdFile = new File(directoryLocation, fileName);
                 if (!createdFile.exists()) {
                     throw new FileNotFoundException(fileName + " not found in directory " + directoryLocation.getName());
                 }
-                files[i - STARTING_EXPONENT] = new File(directoryLocation, fileName);
+                files[i - startExponent] = new File(directoryLocation, fileName);
             }
         }
         return files;
     }
+
+
 }
