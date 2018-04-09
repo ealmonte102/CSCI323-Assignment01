602d778b480a0f1a43ecdb4fc4d98e4b23c66701
Evan Almonte <ealmonte102@qc.cuny.edu>
2018-04-09 13:19:24 -0400

    Add method to retrieve and set the current list to be printed

--- src/com/alev/csci323/assignment1/file/SimpleSortedListWriter.java
+++ src/com/alev/csci323/assignment1/file/SimpleSortedListWriter.java
@@ -6,11 +6,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class SimpleSortedListWriter implements SortedListWriter {
-    private final List<Integer> listToBeWritten;
-
-    public SimpleSortedListWriter(List<Integer> listToBeWritten) {
-        this.listToBeWritten = Objects.requireNonNull(listToBeWritten);
-    }
+    private List<Integer> listToBeWritten;
 
     @Override
     public void writeList(Writer writer) throws IOException {
@@ -18,5 +14,13 @@ public class SimpleSortedListWriter implements SortedListWriter {
             writer.write(element.toString() + '\n');
         }
         writer.flush();
+    }
+
+    public List<Integer> getList() {
+        return listToBeWritten;
+    }
+
+    public void setList(List<Integer> listToBeWritten) {
+        this.listToBeWritten = Objects.requireNonNull(listToBeWritten);
     }
 }
